3980ef54533aebef0cdfcb4df9475e08c92d32fd
Zachary Berardo <zachary@zachnthebox.com>
2024-11-17 09:57:23 -0500

    fix: get rid of mosquitto permission warnings

--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 touch /mosquitto/config/passwd;
-chown root /mosquitto/config/passwd;
-mosquitto_passwd -b /mosquitto/config/passwd $DEFAULT_USERNAME $DEFAULT_PASSWORD;
+chown root:root /mosquitto/config/passwd;
 chmod 0700 /mosquitto/config/passwd;
+mosquitto_passwd -b /mosquitto/config/passwd $DEFAULT_USERNAME $DEFAULT_PASSWORD;
 chown mosquitto:mosquitto /mosquitto/config/passwd;
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf;
